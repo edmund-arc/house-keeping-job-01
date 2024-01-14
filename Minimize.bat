5e206453a836abd6770e815dfb323cc8c6b5a00e
edmund-arc <154868349+edmund-arc@users.noreply.github.com>
2024-01-14 22:32:00 +0800

    Update Minimize.bat

--- Minimize.bat
+++ Minimize.bat
@@ -1,4 +1,6 @@
 # added a batch file to minimise to windows background
+# so this will start the minimixze
+
 start /min C:\RiderGate\net7.0\BackUpApp.exe
 
 start /min C:\RiderKiosk\net7.0\BackUpApp.exe
